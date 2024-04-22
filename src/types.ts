5c144966b65e50ec51e5eba825f67a7a383f4cd2
mohammadbagheri228@gmail.com <mohammadbagheri228@gmail.com>
2024-04-22 20:57:21 +0330

    refactor: :label: use modular types

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,19 @@
+import { Dispatch, SetStateAction } from "react";
+
 export enum STEP_MODAL {
   "wallet" = "wallet",
   "login" = "login",
+  "otp" = "otp",
   "register" = "register",
   "connectors" = "connectors",
+}
+
+export interface WalletModalBodyProps {
+  setStep: Dispatch<SetStateAction<STEP_MODAL>>;
+}
+
+export interface ModalForm {
+  email: string;
+  password: string;
+  isAccepted?: boolean;
 }
