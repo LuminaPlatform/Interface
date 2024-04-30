dd47190099877061e72585632dd73af37398cd08
MohammadHossein Bagheri <mohammadbagheri228@gmail.com>
2024-05-01 00:16:44 +0330

    feat: :sparkles: craeted toast config hook

--- src/hooks/bases.tsx
+++ src/hooks/bases.tsx
@@ -5,6 +5,7 @@ import {
   SetSelectedProjects,
   WalletConnectData,
 } from "@/context";
+import { useToast, UseToastOptions } from "@chakra-ui/react";
 import { useContext } from "react";
 
 export const useIsOpenSidebar = () => useContext(IsSidebarOpen);
@@ -15,3 +16,10 @@ export const useWalletModal = () => useContext(WalletConnectData);
 export const useSelectedProjects = () => useContext(SelectedProjects);
 export const useDispatchSelectedProjects = () =>
   useContext(SetSelectedProjects);
+
+export const useCustomToast = (args?: UseToastOptions) =>
+  useToast({
+    position: "bottom-left",
+    duration: 3000,
+    ...args,
+  });
