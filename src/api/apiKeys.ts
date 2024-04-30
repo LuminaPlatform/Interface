d80b02a7168773fe4afe07d6fff42ded0b47edbe
MohammadHossein Bagheri <mohammadbagheri228@gmail.com>
2024-05-01 00:14:38 +0330

    feat: :alien: added api keys

---
+++ src/api/apiKeys.ts
@@ -0,0 +1,17 @@
+export const apiKeys = {
+  auth: {
+    login: {
+      google: "",
+      twitter: "",
+      email: "/auth/login/email",
+      wallet: "",
+    },
+    signup: {
+      google: "",
+      twitter: "",
+      email: "/auth/signup/email/request",
+      wallet: "",
+    },
+    otp: "/auth/signup/email/verify",
+  },
+};
