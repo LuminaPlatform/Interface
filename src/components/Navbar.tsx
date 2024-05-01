fb742e76a54eb7f423c1c7d391382afd70684a63
MohammadHossein Bagheri <mohammadbagheri228@gmail.com>
2024-05-02 01:16:00 +0330

    feat: :sparkles: added profile menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   Alert,
   AlertIcon,
@@ -8,14 +9,118 @@ import {
   Input,
   InputGroup,
   InputLeftElement,
+  Menu,
+  MenuButton,
+  MenuList,
   Text,
-  useDisclosure,
+  VStack,
 } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { ConnectModal } from "./modals/Connect";
-import { TbBell, TbSearch } from "react-icons/tb";
+import {
+  TbBell,
+  TbLogout,
+  TbSearch,
+  TbSettings2,
+  TbUserCircle,
+} from "react-icons/tb";
 import { useWalletModal } from "@/hooks/bases";
 import { useAccount } from "wagmi";
+import Link from "next/link";
+
+const ProfileBox = () => {
+  return (
+    <Menu>
+      <MenuButton as={Box} display="flex">
+        <HStack width="full">
+          <Img
+            rounded="full"
+            width="32px"
+            height="32px"
+            border="1px solid"
+            borderColor="gray.0"
+            src="/assets/images/default-img.png"
+            alt="user"
+          />
+          <Text color="gray.10" fontSize="md" fontWeight="200">
+            Anonyms
+          </Text>
+        </HStack>
+      </MenuButton>
+      <MenuList
+        border="none"
+        width="290px"
+        px="16px"
+        py="24px"
+        bg="gray.900"
+        zIndex={2}
+        cursor="default"
+      >
+        <HStack columnGap="8px" mb="16px" rowGap="8px">
+          <Img
+            rounded="full"
+            width="64px"
+            height="64px"
+            src="/assets/images/default-img.png"
+            alt="profile"
+          />
+          <Text
+            fontFamily="lexend"
+            color="gray.0"
+            fontSize="xl"
+            fontWeight="600"
+          >
+            Anonyms
+          </Text>
+        </HStack>
+        <VStack rowGap="24px">
+          <Button
+            fontSize="md"
+            fontWeight="700"
+            height="48px"
+            variant="primaryDark"
+            width="full"
+            as={Link}
+            href="/profile"
+          >
+            <HStack width="full" justifyContent="flex-start" columnGap="8px">
+              <TbUserCircle fontSize="20px" />
+              <Text>My Profile</Text>
+            </HStack>
+          </Button>
+          <Button
+            fontSize="md"
+            fontWeight="700"
+            height="48px"
+            variant="primaryDark"
+            width="full"
+            as={Link}
+            href="/settings"
+          >
+            <HStack width="full" justifyContent="flex-start" columnGap="8px">
+              <TbSettings2 fontSize="20px" />
+              <Text>Account Setting</Text>
+            </HStack>
+          </Button>
+          <Button
+            fontSize="md"
+            fontWeight="700"
+            height="48px"
+            variant="primaryDark"
+            width="full"
+            as={Link}
+            href="/settings"
+          >
+            <HStack width="full" justifyContent="flex-start" columnGap="8px">
+              <TbLogout fontSize="20px" />
+              <Text>Log Out</Text>
+            </HStack>
+          </Button>
+        </VStack>
+      </MenuList>
+    </Menu>
+  );
+};
 
 const Navbar = () => {
   const [search, setSearch] = useState("");
@@ -115,7 +220,7 @@ const Navbar = () => {
             <TbBell size={24} color="var(--chakra-colors-gray-0)" />
           </Box>
         )}
-        <HStack cursor="pointer" columnGap="8px">
+        <HStack flexWrap="nowrap" cursor="pointer" columnGap="8px">
           {!isConnected ? (
             <Button
               onClick={onOpen}
@@ -127,20 +232,7 @@ const Navbar = () => {
               Connect
             </Button>
           ) : (
-            <>
-              <Img
-                rounded="full"
-                width="32px"
-                height="32px"
-                border="1px solid"
-                borderColor="gray.0"
-                src="/assets/images/default-img.png"
-                alt="user"
-              />
-              <Text color="gray.10" fontSize="md" fontWeight="200">
-                Anonyms
-              </Text>
-            </>
+            <ProfileBox />
           )}
         </HStack>
       </HStack>
