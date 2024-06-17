a64ab4810890b788692a0029d84d00b3a7e835c8
MohammadHossein Bagheri <mohammadbagheri228@gmail.com>
2024-06-17 13:58:18 +0330

    refactor: :speech_balloon: changed copyrights

--- src/modules/reviews/components/TabBody.tsx
+++ src/modules/reviews/components/TabBody.tsx
@@ -7,14 +7,13 @@ import { useReviewsData } from "../hooks";
 const TabBody = () => {
   const reviews = useReviewsData();
 
-
   return (
     <VStack rowGap="16px" py="16px">
       {reviews.length === 0 ? (
         <EmptyPage
-          description="Looks like you haven't followed anyone yet! To see reviews, explore
-        in other categories. Expand your network and discover insightful reviews
-        from trusted voices."
+          description="Looks like you haven’t followed anyone yet! To see reviews, explore in other categories.
+If you have followed users, they might not have posted any reviews yet.
+Expand your network and discover insightful reviews from trusted voices"
           header="Start Following for Reviews"
           imgSrc="/assets/images/empty_reviews.png"
         />
