3c29947d5aff33ee3f8096e4b2f0d804616d53ea
MohammadHossein Bagheri <mohammadbagheri228@gmail.com>
2024-07-09 17:11:17 +0330

    style: :lipstick: Fix contribution height and overflow
    Fix contribution height and overflow

---
+++ src/modules/projects/pdp/components/Contribution.tsx
@@ -0,0 +1,211 @@
+"use client";
+import {
+  GridItem,
+  HStack,
+  SimpleGrid,
+  Text,
+  useDisclosure,
+  VStack,
+} from "@chakra-ui/react";
+import Link from "next/link";
+import { TbChevronRight } from "react-icons/tb";
+import { useProjectData } from "../hooks";
+import { useEffect, useRef, useState } from "react";
+import { Reviews } from "./Reviews";
+import { Feedback } from "./Feedback";
+import { ProjectLink } from "./ProjectLink";
+
+export const Contribution = () => {
+  const project = useProjectData();
+  const {
+    id,
+    content: {
+      contributionLinks,
+      contributionDescription,
+      impactDescription,
+      impactMetrics,
+    },
+  } = project;
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const contributionRef = useRef<HTMLDivElement>();
+  const [contributionHeight, setContributionHeight] = useState(
+    contributionRef?.current?.clientHeight
+  );
+  useEffect(() => {
+    setContributionHeight(contributionRef?.current?.clientHeight);
+  }, [contributionRef?.current?.clientHeight]);
+
+  const impactRef = useRef<HTMLDivElement>();
+  const [impactHeight, setImpactHeight] = useState(
+    impactRef?.current?.clientHeight
+  );
+  useEffect(() => {
+    setImpactHeight(impactRef?.current?.clientHeight);
+  }, [impactRef?.current?.clientHeight]);
+
+  console.log({ height: impactRef?.current?.clientHeight });
+
+  return (
+    <VStack width="full">
+      <HStack justifyContent="space-between" width="full">
+        <Text
+          fontFamily="lexend"
+          fontSize="2xl"
+          color="gray.20"
+          fontWeight="600"
+        >
+          Reviews
+        </Text>
+        <HStack>
+          <Text
+            color="primary.200"
+            fontSize="md"
+            fontWeight="700"
+            as={Link}
+            href={`/projects/${id}/reviews`}
+          >
+            Show All Reviews
+          </Text>
+          <TbChevronRight
+            size="16px"
+            color="var(--chakra-colors-primary-200)"
+          />
+        </HStack>
+      </HStack>
+      <SimpleGrid width="full" gap="24px" columns={{ base: 1, lg: 3 }}>
+        <GridItem
+          overflow="auto"
+          maxH="384px"
+          order={{ base: "1", lg: "0" }}
+          colSpan={{ base: 1, lg: 2 }}
+        >
+          <Reviews isOpen={isOpen} onOpen={onOpen} onClose={onClose} />
+        </GridItem>
+        <GridItem
+          order={{ base: "0", lg: "1" }}
+          colSpan={{ base: 1, lg: 1 }}
+          bg="gray.700"
+          borderRadius="10px"
+          p="16px"
+        >
+          <Feedback
+            isOpen={isOpen}
+            onOpen={onOpen}
+            onClose={onClose}
+            headerTitle="Which status best describes your view of the project?"
+          />
+        </GridItem>
+      </SimpleGrid>
+      <SimpleGrid
+        mt="32px"
+        width="full"
+        gap="24px"
+        columns={{ base: 1, lg: 3 }}
+        minH="325px"
+      >
+        <GridItem ref={contributionRef} colSpan={{ base: 1, lg: 2 }}>
+          <VStack rowGap="8px" width="full">
+            <Text
+              color="gray.20"
+              fontSize="2xl"
+              fontWeight="600"
+              fontFamily="lexend"
+              width="full"
+              textAlign="left"
+            >
+              Contribution
+            </Text>
+            <Text color="gray.20" fontSize="md" lineHeight="24px">
+              {contributionDescription}
+            </Text>
+          </VStack>
+        </GridItem>
+        {contributionHeight && (
+          <GridItem
+            overflowY="auto"
+            rowGap="12px"
+            colSpan={{ base: 1, lg: 1 }}
+            maxH={contributionHeight}
+          >
+            <Text
+              color="gray.20"
+              fontSize="xl"
+              fontWeight="600"
+              fontFamily="lexend"
+              mb="12px"
+            >
+              Contribution links
+            </Text>
+            <VStack width="full" rowGap="12px">
+              {contributionLinks.data.map((item) => (
+                <ProjectLink
+                  description={item.description}
+                  url={item.url}
+                  key={item.url}
+                  showCount={false}
+                  type={item.type}
+                />
+              ))}
+            </VStack>
+          </GridItem>
+        )}
+      </SimpleGrid>
+      <SimpleGrid
+        mt="32px"
+        width="full"
+        gap="24px"
+        columns={{ base: 1, lg: 3 }}
+        minH="325px"
+      >
+        <GridItem colSpan={{ base: 1, lg: 2 }}>
+          <VStack ref={impactRef} rowGap="8px" width="full">
+            <Text
+              color="gray.20"
+              fontSize="2xl"
+              fontWeight="600"
+              fontFamily="lexend"
+              width="full"
+              textAlign="left"
+              mb="12px"
+            >
+              Impact
+            </Text>
+            <Text color="gray.20" fontSize="md" lineHeight="24px">
+              {impactDescription}
+            </Text>
+          </VStack>
+        </GridItem>
+        {impactHeight && (
+          <GridItem
+            maxHeight={impactHeight}
+            overflowY="auto"
+            rowGap="12px"
+            colSpan={{ base: 1, lg: 1 }}
+          >
+            <Text
+              color="gray.20"
+              fontSize="xl"
+              fontWeight="600"
+              fontFamily="lexend"
+              mb="12px"
+            >
+              Impact Metrics
+            </Text>
+            <VStack width="full" rowGap="12px">
+              {impactMetrics.data.map((item) => (
+                <ProjectLink
+                  key={item.url}
+                  description={item.description}
+                  showCount
+                  url={item.url}
+                  count={item.number}
+                />
+              ))}
+            </VStack>
+          </GridItem>
+        )}
+      </SimpleGrid>
+    </VStack>
+  );
+};
--- src/modules/projects/pdp/page/Index.tsx
+++ src/modules/projects/pdp/page/Index.tsx
@@ -8,28 +8,20 @@ import {
   VStack,
 } from "@chakra-ui/react";
 import { Header } from "../components/Header";
-import { useProjectData } from "../hooks";
 import { Hero } from "../components/Hero";
-import { Reviews } from "../components/Reviews";
-import { Feedback } from "../components/Feedback";
-import Link from "next/link";
-import { TbChevronRight } from "react-icons/tb";
-import { ProjectLink } from "../components/ProjectLink";
+import dynamic from "next/dynamic";
+
+const Contribution = dynamic(
+  () =>
+    import("../components/Contribution").then(
+      (modules) => modules.Contribution
+    ),
+  {
+    ssr: false,
+  }
+);
 
 export const Index = () => {
-  const project = useProjectData();
-  const {
-    id,
-    content: {
-      contributionLinks,
-      contributionDescription,
-      impactDescription,
-      impactMetrics,
-    },
-  } = project;
-
-  const { isOpen, onOpen, onClose } = useDisclosure();
-
   return (
     <VStack
       pb="16px"
@@ -60,150 +52,7 @@ export const Index = () => {
         p="24px"
       >
         <Hero />
-        <VStack width="full">
-          <HStack justifyContent="space-between" width="full">
-            <Text
-              fontFamily="lexend"
-              fontSize="2xl"
-              color="gray.20"
-              fontWeight="600"
-            >
-              Reviews
-            </Text>
-            <HStack>
-              <Text
-                color="primary.200"
-                fontSize="md"
-                fontWeight="700"
-                as={Link}
-                href={`/projects/${id}/reviews`}
-              >
-                Show All Reviews
-              </Text>
-              <TbChevronRight
-                size="16px"
-                color="var(--chakra-colors-primary-200)"
-              />
-            </HStack>
-          </HStack>
-          <SimpleGrid width="full" gap="24px" columns={{ base: 1, lg: 3 }}>
-            <GridItem
-              overflow="auto"
-              maxH="384px"
-              order={{ base: "1", lg: "0" }}
-              colSpan={{ base: 1, lg: 2 }}
-            >
-              <Reviews isOpen={isOpen} onOpen={onOpen} onClose={onClose} />
-            </GridItem>
-            <GridItem
-              order={{ base: "0", lg: "1" }}
-              colSpan={{ base: 1, lg: 1 }}
-              bg="gray.700"
-              borderRadius="10px"
-              p="16px"
-            >
-              <Feedback
-                isOpen={isOpen}
-                onOpen={onOpen}
-                onClose={onClose}
-                headerTitle="Which status best describes your view of the project?"
-              />
-            </GridItem>
-          </SimpleGrid>
-          <SimpleGrid
-            mt="32px"
-            width="full"
-            gap="24px"
-            columns={{ base: 1, lg: 3 }}
-          >
-            <GridItem colSpan={{ base: 1, lg: 2 }}>
-              <VStack rowGap="8px" width="full">
-                <Text
-                  color="gray.20"
-                  fontSize="2xl"
-                  fontWeight="600"
-                  fontFamily="lexend"
-                  width="full"
-                  textAlign="left"
-                >
-                  Contribution
-                </Text>
-                <Text color="gray.20" fontSize="md" lineHeight="24px">
-                  {contributionDescription}
-                </Text>
-              </VStack>
-            </GridItem>
-            <GridItem maxH="352px" overflowY='auto' rowGap="12px" colSpan={{ base: 1, lg: 1 }}>
-              <Text
-                color="gray.20"
-                fontSize="xl"
-                fontWeight="600"
-                fontFamily="lexend"
-                mb="12px"
-              >
-                Contribution links
-              </Text>
-              <VStack width="full" rowGap="12px">
-                {contributionLinks.data.map((item) => (
-                  <ProjectLink
-                    description={item.description}
-                    url={item.url}
-                    key={item.url}
-                    showCount={false}
-                    type={item.type}
-                  />
-                ))}
-              </VStack>
-            </GridItem>
-          </SimpleGrid>
-          <SimpleGrid
-            mt="32px"
-            width="full"
-            gap="24px"
-            columns={{ base: 1, lg: 3 }}
-          >
-            <GridItem colSpan={{ base: 1, lg: 2 }}>
-              <VStack rowGap="8px" width="full">
-                <Text
-                  color="gray.20"
-                  fontSize="2xl"
-                  fontWeight="600"
-                  fontFamily="lexend"
-                  width="full"
-                  textAlign="left"
-                  mb="12px"
-                >
-                  Impact
-                </Text>
-                <Text color="gray.20" fontSize="md" lineHeight="24px">
-                  {impactDescription}
-                </Text>
-              </VStack>
-            </GridItem>
-            <GridItem maxH="352px" overflowY='auto' rowGap="12px" colSpan={{ base: 1, lg: 1 }}>
-              <Text
-                color="gray.20"
-                fontSize="xl"
-                fontWeight="600"
-                fontFamily="lexend"
-                mb="12px"
-              >
-                Impact Metrics
-              </Text>
-              <VStack width="full" rowGap="12px">
-                {impactMetrics.data.map((item) => (
-                  <ProjectLink
-                    key={item.url}
-                    description={item.description}
-                    showCount
-                    url={item.url}
-                    count={item.number}
-                  />
-                ))}
-              </VStack>
-            </GridItem>
-          </SimpleGrid>
-        </VStack>
+        <Contribution />
       </VStack>
     </VStack>
   );
