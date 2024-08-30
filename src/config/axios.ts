0adc4adaecb6c39d73c15b93d3d4f6599483549f
MohammadHossein Bagheri <mohammadbagheri228@gmail.com>
2024-08-30 13:58:00 +0330

    feat: :sparkles: Check the start script by add node env

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,11 +1,16 @@
-import { getBrowserCookie } from "@/utils";
+import { ACCESS_TOKEN_COOKIE_KEY } from "@/constant";
 import axios from "axios";
+import { getCookie } from "cookies-next";
 
 export const axiosClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BASE_API_URL,
+  baseURL: process.env.production
+    ? process.env.NEXT_PUBLIC_BASE_API_URL
+    : process.env.NEXT_PUBLIC_DEV_BASE_API_URL,
   headers: {
     "Content-Type": "application/json",
-    Authorization: `Bearer ${getBrowserCookie("access_token")}`
+    ...(getCookie(ACCESS_TOKEN_COOKIE_KEY) && {
+      Authorization: `Bearer ${getCookie(ACCESS_TOKEN_COOKIE_KEY)}`
+    })
   }
 });
 
