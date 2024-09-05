7481702fc1256ab3139afba628a24b1e2d9aa112
MohammadHossein Bagheri <mohammadbagheri228@gmail.com>
2024-09-05 13:36:59 +0330

    refactor: :recycle: CChange 500 error page

--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -1,3 +1,49 @@
+import { Button, VStack, Img, Text, HStack } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { ReactElement } from "react";
+
 export default function Custom500() {
-  return <h1>500 - Server-side error occurred</h1>;
+  const router = useRouter();
+
+  return (
+    <VStack
+      width="full"
+      position="relative"
+      minH="100vh"
+      justifyContent="center"
+    >
+      <VStack rowGap="24px" position="relative" zIndex={1}>
+        <Img src="/assets/images/500/500.png" />
+        <Text
+          color="gray.0"
+          fontSize="3xl"
+          fontWeight="600"
+          fontFamily="lexend"
+        >
+          Internal Server Error
+        </Text>
+        <Text
+          textAlign="center"
+          maxW={{ base: "300px", md: "485px" }}
+          fontSize="lg"
+          color="gray.0"
+          fontWeight="500"
+        >
+          Oh no! Something went unexpectedly wrong on our end. Rest assured,
+          we&apos;re fixing it ASAP.
+        </Text>
+        <HStack columnGap="24px">
+          <Button onClick={() => router.reload()} variant="outline">
+            Try Again
+          </Button>
+          <Button onClick={() => router.replace("/")} variant="primary">
+            Back to Home
+          </Button>
+        </HStack>
+      </VStack>
+    </VStack>
+  );
 }
+Custom500.getLayout = function getLayout(page: ReactElement) {
+  return <>{page}</>;
+};
