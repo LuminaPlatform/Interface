619e9a949daa18cc2c98b630b4a7a4858316cf8b
MohammadHossein Bagheri <mohammadbagheri228@gmail.com>
2024-09-11 23:17:03 +0330

    feat: :sparkles: Add sentry to lumina

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://7a98bd8ee05499505b6cd03befe1dc95@o4507934528503808.ingest.de.sentry.io/4507934532894800",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false
+});
