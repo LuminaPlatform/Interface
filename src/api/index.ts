cc815dac33ea486031e8d69bfe7ec8211572fda1
MohammadHossein Bagheri <mohammadbagheri228@gmail.com>
2024-09-18 01:16:10 +0330

    refactor: :recycle: Refactor user information by add email is public checking

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,30 +1,89 @@
 import { axiosClient } from "@/config/axios";
 import { apiKeys } from "./apiKeys";
 
-export const getUserInformation = async (id: string, accessToken: string) => {
+export const checkUserEmailIsPublic = (userId: string) =>
+  axiosClient
+    .post(apiKeys.fetch, {
+      "0": {
+        model: "User",
+        model_id: "None",
+        limit: 1,
+        orders: [],
+        graph: {
+          fetch_fields: [
+            {
+              name: "email_public"
+            }
+          ]
+        },
+        condition: {
+          field: "id",
+          operator: "EQ",
+          value: userId,
+          __type__: "SimpleFetchCondition"
+        }
+      }
+    })
+    .then((res) => res.data[0]?.[0]?.email_public);
+
+export const getUserInformation = async (
+  id: string,
+  accessToken: string,
+  isSelfUser: boolean
+) => {
+  const isEmailPublic = await checkUserEmailIsPublic(id);
   return axiosClient
     .post(
       apiKeys.fetch,
       {
-        0: {
-          model: "User",
-          model_id: "None",
-          limit: 1,
-          orders: [],
-          graph: {
-            fetch_fields: [
-              {
-                name: "*"
+        0: (await (isEmailPublic || isSelfUser))
+          ? {
+              model: "User",
+              model_id: "None",
+              limit: 1,
+              orders: [],
+              graph: {
+                fetch_fields: [
+                  {
+                    name: "*"
+                  }
+                ]
+              },
+              condition: {
+                field: "id",
+                operator: "EQ",
+                value: id,
+                __type__: "SimpleFetchCondition"
               }
-            ]
-          },
-          condition: {
-            field: "id",
-            operator: "EQ",
-            value: id,
-            __type__: "SimpleFetchCondition"
-          }
-        },
+            }
+          : {
+              model: "User",
+              model_id: "None",
+              limit: 1,
+              orders: [],
+              graph: {
+                fetch_fields: [
+                  { name: "id" },
+                  { name: "email_public" },
+                  { name: "display_name" },
+                  { name: "profile_id" },
+                  { name: "pined_wallet_id" },
+                  { name: "status" },
+                  { name: "terms_of_service_signature" },
+                  { name: "info" },
+                  { name: "createTimestamp" },
+                  { name: "setting" },
+                  { name: "x_username" },
+                  { name: "username" }
+                ]
+              },
+              condition: {
+                field: "id",
+                operator: "EQ",
+                value: id,
+                __type__: "SimpleFetchCondition"
+              }
+            },
         1: {
           model: "Wallet",
           model_id: "None",
@@ -50,9 +109,18 @@ export const getUserInformation = async (id: string, accessToken: string) => {
           orders: [],
           graph: {
             fetch_fields: [
-              {
-                name: "*"
-              }
+              { name: "id" },
+              { name: "email_public" },
+              { name: "display_name" },
+              { name: "profile_id" },
+              { name: "pined_wallet_id" },
+              { name: "status" },
+              { name: "terms_of_service_signature" },
+              { name: "info" },
+              { name: "createTimestamp" },
+              { name: "setting" },
+              { name: "x_username" },
+              { name: "username" }
             ]
           }
         },
@@ -62,9 +130,18 @@ export const getUserInformation = async (id: string, accessToken: string) => {
           orders: [],
           graph: {
             fetch_fields: [
-              {
-                name: "*"
-              }
+              { name: "id" },
+              { name: "email_public" },
+              { name: "display_name" },
+              { name: "profile_id" },
+              { name: "pined_wallet_id" },
+              { name: "status" },
+              { name: "terms_of_service_signature" },
+              { name: "info" },
+              { name: "createTimestamp" },
+              { name: "setting" },
+              { name: "x_username" },
+              { name: "username" }
             ]
           }
         },
