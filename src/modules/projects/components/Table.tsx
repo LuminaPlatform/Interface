6562f6118ace25cac44e2bddd6bdd437cc6aa712
MohammadHossein Bagheri <mohammadbagheri228@gmail.com>
2024-09-21 21:34:56 +0330

    fix: :bug: Change pagination click handler

--- src/modules/projects/components/Table.tsx
+++ src/modules/projects/components/Table.tsx
@@ -127,6 +127,7 @@ export const ArrowDown = () => (
 );
 
 const Table = ({ search }: { search: string }) => {
+  const [isPaginationLoading, setPaginationLoading] = useState(false);
   const router = useRouter();
   const page = router.query?.page ?? 1;
   const projectsData = useProjects();
@@ -409,10 +410,18 @@ const Table = ({ search }: { search: string }) => {
         <HStack alignItems="center" width="full" justifyContent="center">
           <Flex columnGap="10px" alignItems="center">
             <Button
+              isDisabled={+page === 1 || isPaginationLoading}
               onClick={() => {
-                // if (page !== limit) {
-                router.push(`/projects?page=${+page + 1}`);
-                // }
+                setPaginationLoading(true);
+                if (+page !== 1) {
+                  router
+                    .push(`/projects?page=${+page - 1}`, undefined, {
+                      scroll: false
+                    })
+                    .then(() => {
+                      setPaginationLoading(false);
+                    });
+                }
               }}
               variant="ghost"
             >
@@ -422,11 +431,18 @@ const Table = ({ search }: { search: string }) => {
               {page}
             </Text>
             <Button
-              isDisabled={+page === 1}
+              isDisabled={isPaginationLoading}
               onClick={() => {
-                if (+page !== 1) {
-                  router.push(`/projects?page=${+page - 1}`);
-                }
+                setPaginationLoading(true);
+                // if (page !== limit) {
+                router
+                  .push(`/projects?page=${+page + 1}`, undefined, {
+                    scroll: false
+                  })
+                  .then(() => {
+                    setPaginationLoading(false);
+                  });
+                // }
               }}
               variant="ghost"
             >
